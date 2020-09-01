388e6478f7f7acc1ae1f4e030eca27d2d51f6795
Joan' Araujo <wisdombthegame@gmail.com>
2020-09-01 03:49:21 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,26 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)
+i = 0
+while i < array.length
+  yield(words[[i])
+  i = i + 1
+  end
+  words
 end
+
+#def hello_t(array)
+ # i = 0
+ 
+#  while i < array.length
+ #   yield(array[i])
+  #  i = i + 1
+  #end
+ 
+  array
+end
+
+#mechanism
+# A. accept an argument 
+#    of an array and use 
+#   the while loop to iterate over each member of array
+#  Yielding each element contained in the array to a block.
+
