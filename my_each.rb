ded688daf9f99d2f78451b7fd8d2d4c60c6ebe37
Joan' Araujo <wisdombthegame@gmail.com>
2020-09-01 03:52:32 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -7,20 +7,3 @@ end
  words
 end
 
-#def hello_t(array)
- # i = 0
- 
-#  while i < array.length
- #   yield(array[i])
-  #  i = i + 1
-  #end
- 
-  #array
-#end
-
-#mechanism
-# A. accept an argument 
-#    of an array and use 
-#   the while loop to iterate over each member of array
-#  Yielding each element contained in the array to a block.
-
